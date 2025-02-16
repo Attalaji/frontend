429ce6ff0580f0e2a6daa898110220c0590d78e1
badryansah <badryansah99@gmail.com>
2025-02-16 16:37:02 +0800

    Terbaru farin

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import Logo from '../Asset/Logomysteak.jpg';
+import Logo from '../Asset/Assetnavbar/Logomysteak.jpg';
 
 const Navbar = () => {
   return (
